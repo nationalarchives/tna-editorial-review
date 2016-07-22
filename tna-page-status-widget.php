1499ad0adbe7d09f202eefea02b8040461679102
Chris Bishop <domingobishop@gmail.com>
2016-07-22 14:48:48 +0100

    first commit

---
+++ tna-page-status-widget.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: TNA page status widget
+ * Plugin URI: https://github.com/nationalarchives/tna-page-status-widget
+ * Description: Displays pages with pending and draft statuses on the dashboard.
+ * Version: 0.1
+ * Author: Chris Bishop
+ * Author URI: https://github.com/nationalarchives
+ * License: GPL2
+ */
