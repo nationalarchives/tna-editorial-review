1ad80f26f89f11ea6144862458592b499ffdf596
Gwyn Jones <gwyn.jones@nationalarchives.gsi.gov.uk>
2016-07-25 14:13:32 +0100

    Introduce Composer and PHPUnit

---
+++ tests/StatusWidgetTest.php
@@ -0,0 +1,10 @@
+<?php
+
+class StatusWidgetTest extends PHPUnit_Framework_TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+        $this->assertFalse(false);
+    }
+}
