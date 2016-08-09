59de79ce768fdd14e008223b0b11329489eedec4
Chris Bishop <domingobishop@gmail.com>
2016-08-09 12:03:06 +0100

    renamed plugin

---
+++ tna-editorial-review.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Plugin Name: TNA Editorial review
+ * Plugin URI: https://github.com/nationalarchives/tna-editorial-reivew
+ * Description: The National Archives editorial review workflow plugin.
+ * Version: 0.1
+ * Author: Chris Bishop
+ * Author URI: https://github.com/nationalarchives
+ * License: GPL2
+ */
+
+// Loads admin CSS
+function load_tna_page_status_admin_style() {
+	wp_register_style( 'custom_wp_admin_css', plugin_dir_url(__FILE__) . '/style.css', false, '0.1' );
+	wp_enqueue_style( 'custom_wp_admin_css' );
+}
+add_action( 'admin_enqueue_scripts', 'load_tna_page_status_admin_style' );
+
+// Adds dashboard column option to screen options
+function dashboard_columns() {
+	add_screen_option(
+		'layout_columns',
+		array(
+			'max'     => 3,
+			'default' => 2
+		)
+	);
+}
+add_action( 'admin_head-index.php', 'dashboard_columns' );
+
+include 'functions.php';
+include 'tna-page-status-widget.php';
--- tna-page-status-widget.php
+++ tna-page-status-widget.php
@@ -1,22 +1,4 @@
 <?php
-/**
- * Plugin Name: TNA page status widget
- * Plugin URI: https://github.com/nationalarchives/tna-page-status-widget
- * Description: Displays pages with pending and draft statuses on the dashboard.
- * Version: 0.1
- * Author: Chris Bishop
- * Author URI: https://github.com/nationalarchives
- * License: GPL2
- */
-
-include 'functions.php';
-
-// Loads admin CSS
-function load_tna_page_status_admin_style() {
-	wp_register_style( 'custom_wp_admin_css', plugin_dir_url(__FILE__) . '/style.css', false, '0.1' );
-	wp_enqueue_style( 'custom_wp_admin_css' );
-}
-add_action( 'admin_enqueue_scripts', 'load_tna_page_status_admin_style' );
 
 // Adds widget to dashboard
 function page_status_add_dashboard_widgets() {
@@ -27,18 +9,6 @@ function page_status_add_dashboard_widgets() {
 	);
 }
 add_action( 'wp_dashboard_setup', 'page_status_add_dashboard_widgets' );
-
-// Adds dashboard column option to screen options
-function dashboard_columns() {
-	add_screen_option(
-		'layout_columns',
-		array(
-			'max'     => 3,
-			'default' => 2
-		)
-	);
-}
-add_action( 'admin_head-index.php', 'dashboard_columns' );
 
 // Page status function
 function page_status_dashboard_widget_function() {
