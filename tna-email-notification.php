efeb94b566c153a548c27dd268f48b84a59ad840
Chris Bishop <domingobishop@gmail.com>
2016-08-10 16:17:03 +0100

    save email notification meta data in user profile and get subscribed users email addresses

--- tna-email-notification.php
+++ tna-email-notification.php
@@ -33,6 +33,16 @@ function user_email_notifications( $user ) {
 add_action( 'show_user_profile', 'user_email_notifications' );
 add_action( 'edit_user_profile', 'user_email_notifications' );
 
+function save_user_email_notifications( $user_id ) {
+	if ( !current_user_can( 'edit_user', $user_id ) )
+		return false;
+	if( !isset($_POST['notification']) )
+		$_POST['notification'] = 'false';
+	update_user_meta( $user_id, 'notification', sanitize_text_field( $_POST['notification'] ) );
+}
+add_action( 'personal_options_update', 'save_user_email_notifications' );
+add_action( 'edit_user_profile_update', 'save_user_email_notifications' );
+
 function wp_mail_set_text_body($phpmailer) {
 	if (empty($phpmailer->AltBody)) {
 		$phpmailer->AltBody = strip_tags($phpmailer->Body);
@@ -51,14 +61,23 @@ function get_user_changes_comments( $myChanges ) {
 function notify_editor_of_pending( $post ) {
 	$comments = get_user_changes_comments('my-changes');
 	$current_user = wp_get_current_user();
-	$user = $current_user->display_name;
-	$to = array ( 'domingobishop@gmail.com', $current_user->user_email );
-	$subject = 'Editorial review: ' . $user . ' submitted a page for review';
-	$message = '<p><strong>' . $user . '</strong> has submitted <strong>' . get_the_title() . '</strong> for review.</p>';
+	$userName = $current_user->display_name;
+	$subject = 'Editorial review: ' . $userName . ' submitted a page for review';
+	$message = '<p><strong>' . $userName . '</strong> has submitted <strong>' . get_the_title() . '</strong> for review.</p>';
 	$message .= '<p>Page title: ' . get_the_title() . ' <a href="' . wp_get_shortlink() . '&preview=true">Preview</a> <a href="' . get_edit_post_link() . '">Edit</a></p>';
 	$message .= '<p>Modified: ' . get_the_modified_date($d = 'j/n/y G:i') . '</p>';
-	$message .= '<p>' . $user . ' comments: ' . $comments . '</p>';
-	wp_mail( $to, $subject, $message );
+	$message .= '<p>' . $userName . ' comments: ' . $comments . '</p>';
+	$args = array(
+		'meta_query' => array(
+			array( 'key' => 'notification', 'value' => 'true', 'compare' => '=' )
+		),
+		'fields' => array( 'display_name', 'user_email' )
+	);
+	$users = get_users( $args );
+	foreach ($users as $user) {
+		$to = array( $user->user_email, $current_user->user_email );
+		wp_mail( $to, $subject, $message );
+	}
 }
 add_action( 'new_to_pending', 'notify_editor_of_pending' );
 add_action( 'draft_to_pending', 'notify_editor_of_pending' );
