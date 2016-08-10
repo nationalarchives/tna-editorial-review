7dc039bc1f8211ad88e3156a297dce1408a0a139
Chris Bishop <domingobishop@gmail.com>
2016-08-10 15:56:01 +0100

    created get_user_changes_comments function

--- tna-email-notification.php
+++ tna-email-notification.php
@@ -9,8 +9,8 @@ add_action( 'post_submitbox_misc_actions', 'adds_tell_us_what_changes_textarea' 
 function adds_tell_us_what_changes_textarea(){
 	?>
 	<div class="misc-pub-section changes-comment">
-		<label for="changes">Tell us what changes you have made</label><br />
-		<textarea id="changes" name="changes"></textarea>
+		<label for="my-changes">Tell us what changes you have made</label><br />
+		<textarea id="my-changes" name="my-changes"></textarea>
 	</div>
 	<?php
 }
@@ -34,25 +34,34 @@ add_action( 'show_user_profile', 'user_email_notifications' );
 add_action( 'edit_user_profile', 'user_email_notifications' );
 
 function wp_mail_set_text_body($phpmailer) {
-	if (empty($phpmailer->AltBody)) {$phpmailer->AltBody = strip_tags($phpmailer->Body);}
+	if (empty($phpmailer->AltBody)) {
+		$phpmailer->AltBody = strip_tags($phpmailer->Body);
+	}
 }
 add_action('phpmailer_init','wp_mail_set_text_body');
 
-function notify_editor_for_pending( $post ) {
-	$comments = filter_input(INPUT_POST, 'changes', FILTER_SANITIZE_SPECIAL_CHARS);
+function get_user_changes_comments( $myChanges ) {
+	if ($myChanges) {
+		return filter_input(INPUT_POST, $myChanges, FILTER_SANITIZE_SPECIAL_CHARS);
+	} else {
+		return 'No user comments';
+	}
+}
+
+function notify_editor_of_pending( $post ) {
+	$comments = get_user_changes_comments('my-changes');
 	$current_user = wp_get_current_user();
 	$user = $current_user->display_name;
 	$to = array ( 'domingobishop@gmail.com', $current_user->user_email );
 	$subject = 'Editorial review: ' . $user . ' submitted a page for review';
 	$message = '<p><strong>' . $user . '</strong> has submitted <strong>' . get_the_title() . '</strong> for review.</p>';
 	$message .= '<p>Page title: ' . get_the_title() . ' <a href="' . wp_get_shortlink() . '&preview=true">Preview</a> <a href="' . get_edit_post_link() . '">Edit</a></p>';
-	$message .= '<p>Modified: ' . get_the_modified_date($d = 'j/n/y h:i') . '</p>';
+	$message .= '<p>Modified: ' . get_the_modified_date($d = 'j/n/y G:i') . '</p>';
 	$message .= '<p>' . $user . ' comments: ' . $comments . '</p>';
 	wp_mail( $to, $subject, $message );
 }
-
-add_action( 'new_to_pending', 'notify_editor_for_pending' );
-add_action( 'draft_to_pending', 'notify_editor_for_pending' );
-add_action( 'auto-draft_to_pending', 'notify_editor_for_pending' );
-add_action( 'publish_post', 'notify_editor_for_pending' );
-add_action( 'publish_page', 'notify_editor_for_pending' );
+add_action( 'new_to_pending', 'notify_editor_of_pending' );
+add_action( 'draft_to_pending', 'notify_editor_of_pending' );
+add_action( 'auto-draft_to_pending', 'notify_editor_of_pending' );
+add_action( 'publish_post', 'notify_editor_of_pending' );
+add_action( 'publish_page', 'notify_editor_of_pending' );
