87d711798652e2bcbe359db3117716d4ee247219
Chris Bishop <domingobishop@gmail.com>
2016-08-12 18:42:55 +0100

    refactored html email function

--- tna-email-notification.php
+++ tna-email-notification.php
@@ -17,44 +17,66 @@ function adds_tell_us_what_changes_textarea(){
 	<?php }
 }
 
-function wp_mail_set_text_body($phpmailer) {
+function wp_mail_set_text_body( $phpmailer ) {
 	if (empty($phpmailer->AltBody)) {
 		$phpmailer->AltBody = strip_tags($phpmailer->Body);
 	}
 }
 add_action('phpmailer_init','wp_mail_set_text_body');
 
-function html_message() {
+function html_email_subject_pending( $name ) {
+
+	// HTML email footer (This HTML format will only work with Outlook)
+	$subject = 'Editorial review: ' . $name . ' submitted a page for review';
+
+	return $subject;
+}
+
+function html_email_header( $title ) {
+
+	// HTML email header (This HTML format will only work with Outlook)
+	$html_header = '<html><head>';
+	$html_header .= '<title>' . $title . '</title>';
+	$html_header .= '<style type="text/css">body{font-family:Arial,sans-serif;font-size:16px;}a,strong{color:#0073aa;}</style>';
+	$html_header .= '</head><body>';
+
+	return $html_header;
+}
+
+function html_email_footer() {
+
+	// HTML email footer (This HTML format will only work with Outlook)
+	$html_footer = '<hr></body></html>';
+
+	return $html_footer;
+}
+
+function html_email_body( $sender, $title, $shortlink, $edit_link, $page_url, $date ) {
 
 	// Variables
 	global $post;
-	global $current_user;
 	$comments = get_user_changes_comments( filter_input(INPUT_POST, 'my-changes', FILTER_SANITIZE_SPECIAL_CHARS) );
 
 	// Greeting
 	$greetings = array( 'Hello', 'G&lsquo;day', 'Hey', 'Buna', 'Kon&lsquo;nichiwa', 'Bonjour', 'Hola', 'Ciao' );
 
-	// HTML Email
-	$html_message = '<html><head><title>Editorial review: ' . $current_user->display_name . ' submitted a page for review</title>';
-	$html_message .= '<style type="text/css">a{color:#0073aa;}</style></head>';
-	$html_message .= '<body style="font-family:Arial,sans-serif;font-size:16px;">';
-	$html_message .= $greetings[array_rand($greetings, 1)] . ' web editor,';
-	$html_message .= '<h3><strong style="color:#0073aa;">' . $current_user->display_name . '</strong> has submitted ';
-	$html_message .= '<strong style="color:#0073aa;">' . get_the_title() . '</strong> for review</h3>';
-	$html_message .= '<p>Page title: ' . get_the_title() . ' ';
-	$html_message .= '<small>( <a href="' . wp_get_shortlink() . '&preview=true">Preview</a> | ';
-	$html_message .= '<a href="' . get_edit_post_link() . '">Edit</a> )</small></p>';
-	$html_message .= '<p>Page hierarchy: ' . get_permalink() . '</p>';
+	// HTML email body (This HTML format will only work with Outlook)
+	$html_message = '<p>' . $greetings[array_rand($greetings, 1)] . ' web editor,</p>';
+	$html_message .= '<h3><strong>' . $sender . '</strong> has submitted ';
+	$html_message .= '<strong>' . $title . '</strong> for review</h3>';
+	$html_message .= '<p>Page title: ' . $title . ' ';
+	$html_message .= '<small>( <a href="' . $shortlink . '&preview=true">Preview</a> | ';
+	$html_message .= '<a href="' . $edit_link . '">Edit</a> )</small></p>';
+	$html_message .= '<p>Page hierarchy: ' . $page_url . '</p>';
 	$html_message .= '<p>Page ID: ' . $post->ID . '</p>';
-	$html_message .= '<p>Modified: ' . get_the_modified_date($d = 'l d M Y, G:i') . '</p>';
-	$html_message .= '<p>' . $current_user->display_name . ' comments:</p>';
-	$html_message .= '<p>' . $comments . '</p><hr>';
-	$html_message .= '</body></html>';
+	$html_message .= '<p>Modified: ' . $date . '</p>';
+	$html_message .= '<p>' . $sender . ' comments:</p>';
+	$html_message .= '<p>' . $comments . '</p>';
 
 	return $html_message;
 }
 
-function notify_editor_of_pending( $post ) {
+function notify_editor_of_pending() {
 
 	// Current user
 	$current_user = wp_get_current_user();
@@ -63,10 +85,19 @@ function notify_editor_of_pending( $post ) {
 	$to = array( get_web_editor_email( get_userdata(22) ), $current_user->user_email );
 
 	// Email Subject
-	$subject = 'Editorial review: ' . $current_user->display_name . ' submitted a page for review';
+	$subject = html_email_subject_pending( $current_user->display_name );
 
 	// Email message
-	$message = html_message();
+	$message = html_email_header( html_email_subject_pending( $current_user->display_name ) )
+	           . html_email_body(
+		           $current_user->display_name,                 // Sender
+		           get_the_title(),                             // Page title
+		           wp_get_shortlink(),                          // Preview link
+		           get_edit_post_link(),                        // Edit link
+		           get_permalink(),                             // Page URL
+		           get_the_modified_date($d = 'l d M Y, G:i')   // Page modified date
+	           )
+	           . html_email_footer();
 
 	wp_mail( $to, $subject, $message );
 }
