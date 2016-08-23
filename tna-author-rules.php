eb1d25c70cf7e70595d75d399d945392faa35faf
Chris Bishop <domingobishop@gmail.com>
2016-08-23 14:31:48 +0100

    author can't publish when editing page author permissions pending message

--- tna-author-rules.php
+++ tna-author-rules.php
@@ -4,68 +4,63 @@
 function hide_action_button() {
 	global $post;
 	$status = get_post_status( $post->ID );
-	if ( get_current_user_role() == 'author' && $status == 'pending' ) { ?>
-			<style type="text/css" media="screen">
-				#major-publishing-actions {
-					display: none;
-				}
-			</style>
-			<!-- <p>Web editors reviewing</p> -->
-		<?php }
+	$css = '<style type="text/css" media="screen">#major-publishing-actions{display:none;}</style>';
+	if ( get_current_user_role() == 'author' && $status == 'pending' ) {
+		echo $css;
+	}
 }
 add_action( 'admin_head', 'hide_action_button' );
 
+// Adds message when page status is pending
 function adds_save_draft_button(){
 	global $post;
 	$status = get_post_status( $post->ID );
-	if ( get_current_user_role() == 'author' ) {
-		if ( $status == 'new' || $status == 'auto-draft' ) { ?>
-			<div id="minor-publishing-actions">
-				<div id="save-action">
-					<input type="submit" name="save" id="save-post" value="Save Draft" class="button">
-				</div>
-				<div class="clear"></div>
+	if ( get_current_user_role() == 'author' && $status !== 'pending' ) { ?>
+		<div id="draft-action">
+			<div id="save-action">
+				<input type="submit" name="save" id="save-post" value="Save Draft" class="button">
 			</div>
-		<?php }
-	}
+			<div class="clear"></div>
+		</div>
+	<?php }
 }
-// add_action( 'post_submitbox_misc_actions', 'adds_save_draft_button' );
+add_action( 'post_submitbox_misc_actions', 'adds_save_draft_button' );
 
-function adds_save_as_pending_button(){
+// Adds message when page status is pending
+function adds_editors_reviewing_message(){
 	global $post;
 	$status = get_post_status( $post->ID );
-	if ( get_current_user_role() == 'author' ) {
-		if ( $status == 'new' || $status == 'auto-draft' || $status == 'auto-draft' ) { ?>
-			<div id="minor-publishing-actions">
-				<div id="save-action">
-					<input type="submit" name="save" id="save-post" value="Save as Pending" class="button">
-				</div>
-				<div class="clear"></div>
-			</div>
-		<?php }
-	}
+	if ( get_current_user_role() == 'author' && $status == 'pending' ) { ?>
+		<div class="misc-pub-section pending-message">
+			<p><strong>Web editors reviewing</strong></p>
+		</div>
+	<?php }
 }
-// add_action( 'post_submitbox_misc_actions', 'adds_save_as_pending_button' );
+add_action( 'post_submitbox_misc_actions', 'adds_editors_reviewing_message' );
 
-/*add_filter( 'gettext', 'change_publish_button', 10, 2 );
-
+// Changes the action button text when edit page
 function change_publish_button( $translation, $text ) {
-
-if ( $text == 'Publish' || $text == 'Update' )
-	return 'Save Draft';
-
-return $translation;
-}
-
-function dont_publish() {
-global $post;
-$status = get_post_status( $post->ID );
-if ( get_current_user_role() == 'author' ) {
-	if ( $status == 'new' || $status == 'auto-draft' ) {
-		$post = array( 'ID' => $post->ID, 'post_status' => 'draft' );
-		wp_update_post($post);
+	if ( get_current_user_role() == 'author' ) {
+		if ( $text == 'Publish' ) {
+			return 'Update';
+		}
+		return $translation;
 	}
 }
-}
+add_filter( 'gettext', 'change_publish_button', 10, 2 );
 
-add_action('publish_post' , 'dont_publish');*/
+// Author can't publish when updating an existing page
+function change_status( $post_id ) {
+	if ( get_current_user_role() == 'author' ) {
+		remove_action('save_post', 'change_status');
+		wp_update_post(array('ID' => $post_id, 'post_status' => 'draft'));
+		add_action('save_post', 'change_status');
+	}
+}
+add_action('publish_post', 'change_status');
+
+function take_away_publish_permissions() {
+	$role = get_role( 'author' );
+	$role->remove_cap( 'publish_posts' );
+}
+add_action( 'init', 'take_away_publish_permissions' );
