363852ea97f16e665ae415f3bcedf3a8bdad9fb5
Chris Bishop <domingobishop@gmail.com>
2016-08-26 12:31:34 +0100

    removed alternative change status functions

--- tna-author-rules.php
+++ tna-author-rules.php
@@ -46,8 +46,9 @@ function adds_save_draft_button() {
 		</div>
 	<?php }
 }
-add_action( 'post_submitbox_misc_actions', 'adds_save_draft_button' );
+// add_action( 'post_submitbox_misc_actions', 'adds_save_draft_button' );
 
+// Author can't publish when updating an existing page
 function change_post_status( $data ) {
 	if ( get_current_user_role() == 'author' && $data['post_status'] !== 'pending' ) {
 		if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) return;
@@ -56,41 +57,3 @@ function change_post_status( $data ) {
 	return $data;
 }
 add_filter('wp_insert_post_data', 'change_post_status', '99');
-
-// Author can't publish when updating an existing page
-function change_status( $post_id ) {
-	if ( get_current_user_role() == 'author' ) {
-		remove_action('save_post', 'change_status');
-		wp_update_post(array('ID' => $post_id, 'post_status' => 'draft'));
-		add_action('save_post', 'change_status');
-	}
-}
-// add_action('publish_post', 'change_status');
-
-// Author can't publish when updating an existing page
-function change_status_on_update( $post_id ) {
-	if ( get_current_user_role() == 'author' ) {
-		remove_action('save_post', 'change_status_on_update');
-		wp_update_post(array('ID' => $post_id, 'post_status' => 'draft'));
-		add_action('save_post', 'change_status_on_update');
-	}
-}
-// add_action('pre_post_update', 'change_status_on_update');
-
-// Changes the action button text when edit page
-function change_publish_button( $translation, $text ) {
-	if ( get_current_user_role() == 'author' ) {
-		if ( $text == 'Publish' ) {
-			return 'Update';
-		}
-		return $translation;
-	}
-}
-// add_filter( 'gettext', 'change_publish_button', 10, 2 );
-
-// Removes capabilities
-function take_away_publish_permissions() {
-	$role = get_role( 'author' );
-	$role->remove_cap( 'publish_posts' );
-}
-// add_action( 'init', 'take_away_publish_permissions' );
