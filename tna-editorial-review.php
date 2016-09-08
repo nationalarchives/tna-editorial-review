2662877e8cc7b77fecff9d69799ec997123e7061
Chris Bishop <domingobishop@gmail.com>
2016-09-08 13:49:18 +0100

    reordered inc files

--- tna-editorial-review.php
+++ tna-editorial-review.php
@@ -37,6 +37,6 @@ function is_edit_page( $new_edit = null ) {
 }
 
 include 'functions.php';
+include 'tna-author-rules.php';
 include 'tna-email-notification.php';
 include 'tna-page-status-widget.php';
-include 'tna-author-rules.php';
